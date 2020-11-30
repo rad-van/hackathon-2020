e6985786def219794ed0ba2baf4b41644b14fcd7
Jiangbin Zhao <jiangbinz@radware.com>
2020-11-30 13:22:08 -0800

    server: simplify the logger

--- server/log.js
+++ server/log.js
@@ -13,50 +13,16 @@ const logTrans = (() => {
     winston.format.printf((info) => `${info.timestamp} ${info.message}`)
   );
   const trans = [];
-  const node = process.env.NODE_ENV;
-  if (node === 'docker') {
-    // When running inside a docker container, messages sent to the
-    // console (i.e. stdout and stderr) are forwarded to the docker
-    // logs collector.
-    trans.push(new winston.transports.Console(
-      {
-        format: customFormat,
-        level: process.env.LOGGING_LEVEL || 'info',
-        levels: logLevels,
-        json: false,
-        colorize: false,
-      }
-    ));
-  } else {
-    // normal (i.e. undefined) or 'dev'
-    trans.push(new winston.transports.File(
-      {
-        format: customFormat,
-        level: process.env.LOGGING_LEVEL || 'info',
-        levels: logLevels,
-        json: false,
-        filename: path.join(process.env.LOGGING_PATH || '/var/log',
-                            'alamod.log'),
-        maxsize: 1048576, // in bytes, 1MB
-        maxFiles: 10,
-        tailable: true,
-        zippedarchive: false,
-      }
-    ));
-  }
-
-  if (node === 'dev') {
-    trans.push(new winston.transports.Console(
-      {
-        format: customFormat,
-        level: process.env.DEBUG_LOGGING_LEVEL || 'debug',
-        levels: logLevels,
-        json: false,
-        colorize: false,
-        // timestamp: true,
-      }
-    ));
-  }
+  trans.push(new winston.transports.Console(
+    {
+      format: customFormat,
+      level: process.env.DEBUG_LOGGING_LEVEL || 'debug',
+      levels: logLevels,
+      json: false,
+      colorize: false,
+      // timestamp: true,
+    }
+  ));
   return trans;
 })();
 
